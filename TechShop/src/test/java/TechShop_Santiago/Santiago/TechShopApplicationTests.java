f61ae7f7048953d2f52a8db892256f332924cbbc
SgarciaRjs <sgarcia10051@ufide.ac.cr>
2026-05-20 20:09:57 -0600

    Commit 1
    creacion del proyecto
    semana 1 y 2

---
+++ TechShop/src/test/java/TechShop_Santiago/Santiago/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package TechShop_Santiago.Santiago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
